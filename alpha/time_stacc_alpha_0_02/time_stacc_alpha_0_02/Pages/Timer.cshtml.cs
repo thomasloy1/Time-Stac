3ab42961395d71ad160146f03430dc2d77548df6
Thomas Loy <59874741+thomasloy1@users.noreply.github.com>
2020-05-06 13:17:26 -0400

    Added Stuff

---
+++ alpha/time_stacc_alpha_0_02/time_stacc_alpha_0_02/Pages/Timer.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace time_stacc_alpha_0_02.Pages.Shared
+{
+    public class TimerModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
