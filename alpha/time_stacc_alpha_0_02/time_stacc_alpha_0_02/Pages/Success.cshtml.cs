74dee5256ec7a7c6b99fc710197c4ac093591ce8
Thomas Loy <59874741+thomasloy1@users.noreply.github.com>
2020-05-14 15:02:24 -0400

    Added functionality

--- alpha/time_stacc_alpha_0_02/time_stacc_alpha_0_02/Pages/Success.cshtml.cs
+++ alpha/time_stacc_alpha_0_02/time_stacc_alpha_0_02/Pages/Success.cshtml.cs
@@ -1,17 +1,16 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace time_stacc_alpha_0_02
+namespace time_stacc_alpha_0_02.Pages
 {
-    public class Email_SendModel : PageModel
+    public class SuccessModel : PageModel
     {
         public void OnGet()
         {
-
         }
     }
 }
--- alpha/time_stacc_alpha_0_02/time_stacc_alpha_0_02/Pages/Timer.cshtml.cs
+++ alpha/time_stacc_alpha_0_02/time_stacc_alpha_0_02/Pages/Timer.cshtml.cs
@@ -11,8 +11,38 @@ namespace time_stacc_alpha_0_02.Pages.Shared
     [Authorize]
     public class TimerModel : PageModel
     {
-        public void OnGet()
+        [BindProperty]
+        public string time { get; set; }
+
+        public IActionResult OnPost()
         {
+            try
+            {
+                int[] splittime = time.Split(' ').Select(n => Convert.ToInt32(n)).ToArray();
+
+                if (splittime.Length > 3)
+                {
+                    return BadRequest();
+                }
+
+                int hours = splittime[0];
+                int minutes = splittime[1];
+                int seconds = splittime[2];
+            }
+
+            catch (FormatException)
+            {
+                return BadRequest();
+            }
+            catch (OverflowException) 
+            {
+                return new StatusCodeResult(500);
+            }
+
+
+
+
+            return RedirectToPage("./Timer");
         }
     }
 }
